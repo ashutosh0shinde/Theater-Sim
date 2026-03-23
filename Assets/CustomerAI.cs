894ec453428ac0db1f2339d7ab8e8065f278aa9d
ashutosh0shinde <shindeashutosh110@gmail.com>
2026-03-23 21:12:26 +0530

    add customer ticket queue

---
+++ Assets/CustomerAI.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CustomerAI : MonoBehaviour
+{
+    public PositionManager positionManager;
+    private NavMeshAgent agent;
+    public Transform posToMove;
+
+    private void Start()
+    {
+        positionManager = GameObject.Find("PositionManager").GetComponent<PositionManager>();
+        agent  = GetComponent<NavMeshAgent>();
+        ToTicketQueue();
+    }
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            RotateToTarget(posToMove);
+        }
+    }
+    public void ToTicketQueue() // moves to empty ticket queue, sets posToMove to the current queue position
+    {
+        posToMove = positionManager.ReserveTicketQueue(this);
+        agent.SetDestination(posToMove.position);
+    }
+    void RotateToTarget(Transform rot)
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot.rotation, 5 * Time.deltaTime);
+    }
+    public void DestroyAgent()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/TicketQueuePos.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TicketQueuePos : MonoBehaviour
+{
+    public Transform pos;
+    public bool isReserved = false;
+    public CustomerAI customerAI;
+}
